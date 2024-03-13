0389aff943035239615f6aaf22a28c9502cca545
jaychotaliya987 <jaychotaliya987@gmail.com>
2024-03-13 18:18:53 +0100

    Add function to find maximum string

---
+++ 6.Functions/6.6 Getting things out of Function/I/I/I-O params.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+
+// * We are passing everything by reference but we dont want to manipulate inputs so we are making them const. This is to save time and memory.
+
+void max_str(const std::string &str1,const std::string &str2, std::string &output){
+    if(str1 > str2){
+        output = str1;
+    }else{
+        output = str2;
+    }
+}
+
+int main(){
+    std::string str1 ("Apple") ;
+    std::string str2 ("Banana") ;
+    std::string output;
+
+    max_str(str1, str2, output);
+    std::cout << "Max string is: " << output << std::endl;
+    return 0;
+
+        
+}

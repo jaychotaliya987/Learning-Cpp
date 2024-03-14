9f7b1065620946a32bb51c7d9658fd775bfeca67
jaychotaliya987 <jaychotaliya987@gmail.com>
2024-03-14 10:08:44 +0100

    First commit

---
+++ 6.Functions/6.6 Getting things out of Function/Return.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+
+// * When a function returns a value by default with a return statement, The compiler will decide to pass the variable by value or reference. Whichever is more suitable.
+
+std::string max_str(const std::string str1,const std::string str2){
+    std::string result = str1 + str2;
+    return result;
+}
+
+int main(){
+    std::string str1 ("Apple") ;
+    std::string str2 ("Banana") ;
+    std::string output = max_str(str1, str2);
+
+    std::cout << "Concatenated string is: " << output << std::endl;
+    return 0;
+
+}
